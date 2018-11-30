4da1d023b098fc83d0d7fc4ff10775fef9296ac5
hamyongjae <bbb12388@naver.com>
2018-11-30 19:37:48 +0900

    react view

--- react/src/pages/Root.js
+++ react/src/pages/Root.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const Root = () => (
-    <BrowserRouter>
         <App/>
-    </BrowserRouter>
 );
 
 export default Root;
